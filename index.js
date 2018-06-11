6e3f9aead8599d81d1b98b01e04d181a0b54dd2c
vitaliiloboda <vitalii.loboda@gmail.com>
2018-06-11 22:12:20 +0300

    final2

--- index.js
+++ index.js
@@ -1,5 +1,37 @@
 'use strict';
 
-let sharmSeats;
-let hurgadaSeats;
-let tabaSeats;
+let sharmSeats = 15;
+let hurgadaSeats = 25;
+let tabaSeats = 6;
+
+let isEmptySeats;
+
+
+let userSeats = prompt('Введите число необходимых мест');
+
+if (!(Number.isInteger(Number(userSeats))) || userSeats <= 0 || userSeats === null) {
+  alert('Ошибка ввода');
+} else {
+  if (userSeats <= sharmSeats) {
+    isEmptySeats = confirm('Есть нужное число мест в группе sharm. Вы согласны быть в этой группе?');
+    if (isEmptySeats) {
+      alert('Приятного путешествия в группе sharm');
+    } else alert('Нам очень жаль, приходите еще!');
+    } else {
+      if (userSeats <= hurgadaSeats) {
+        isEmptySeats = confirm('Есть нужное число мест в группе hurgada. Вы согласны быть в этой группе?');
+        if (isEmptySeats) {
+          alert('Приятного путешествия в группе hurgada');
+        } else alert('Нам очень жаль, приходите еще!');
+      } else {
+        if (userSeats <= tabaSeats) {
+          isEmptySeats = confirm('Есть нужное число мест в группе taba. Вы согласны быть в этой группе?');
+          if (isEmptySeats) {
+            alert('Приятного путешествия в группе taba');
+          } else alert('Извините, столько мест нет ни в одной группе!');
+      }
+    }
+}
+}
+
+
